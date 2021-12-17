91051d0e0b28f45ecd5a33bb8960186c0c6d24c2
Juan Esteban <juanes0202@gmail.com>
2021-12-17 17:32:11 -0500

    definiendo las variables de entorno a utilizar en paypal

---
+++ app/Traits/ConsumesExternalServices.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Traits;
+
+use GuzzleHttp\Client;
+
+trait ConsumesExternalServices
+{
+
+    public function makeRequest($method, $requestUrl, $queryParams = [], $formParams = [], $headers = [], $isJsonRequest = false)
+    {
+        $client = new Client([
+            'base_uri' => $this->base_uri,
+        ]);
+
+        if (method_exists($this, 'resolveAuthorization')) {
+            $this->resolveAuthorization($queryParams, $formParams, $headers);
+        }
+
+        $response = $client->request($method, $requestUrl, [
+            $isJsonRequest ? 'json' : 'form_params' => $formParams,
+            'headers' => $headers,
+            'query' => $queryParams,
+        ]);
+
+        if (method_exists($this, 'decodeResponse')) {
+            $this->decodeResponse($response);
+        }
+
+        $response = $response->getBody()->getContents();
+
+        return $response;
+    }
+}
