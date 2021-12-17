bc1812ea7b26a0b62163bd8c4cc79e12d1a3cfa1
Juan Esteban <juanes0202@gmail.com>
2021-12-17 14:38:00 -0500

    agregando formulario para validar montos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,25 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card plain-card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Make a payment</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <form action="#" method="post" enctype="multipart/form" id="payment-form">
+                        @csrf
+                        <div class="row">
+                            <div class="col-auto">
+                                <label for="amount">How much you want to pay</label>
+                                <input type="number" name="amount" id="amount" class="form-control" step="0.01" min="5" value="{{ mt_rand(500, 100000) /100 }}">
+                                <small class="form-text text-muted">
+                                    Use values with up to two decimal positions,
+                                    using dot " . "
+                                </small>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="text-center mt-3">
+                            <button type="submit" id="payButton" class="btn btn-primary">Pay Now</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
