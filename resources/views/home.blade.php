736c71f53534ddd6010a80ed20dd87b2364a3b28
Juan Esteban <juanes0202@gmail.com>
2021-12-20 16:01:19 -0500

    realizando pago con validacion adicional por stripe

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card plain-card">
                 <div class="card-header">Make a payment</div>
 
