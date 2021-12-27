96f2023554ed9b51f6c3fc89fccc5276f728861c
Juan Esteban <juanes0202@gmail.com>
2021-12-27 11:09:09 -0500

    agregando prefijos a las variables del formulario de payu para evitar conflictos con los demás metodos de pago

--- resources/views/components/payu-collapse.blade.php
+++ resources/views/components/payu-collapse.blade.php
@@ -7,24 +7,24 @@
 <div class="form-group form-row">
 
     <div class="col-4">
-        <input type="text" class="form-control" name="card" placeholder="Card Number">
+        <input type="text" class="form-control" name="payu_card" placeholder="Card Number">
     </div>
 
     <div class="col-2">
-        <input type="text" class="form-control" name="cvc" placeholder="CVC">
+        <input type="text" class="form-control" name="payu_cvc" placeholder="CVC">
     </div>
 
 
     <div class="col-2 col-sm-2 col-md-2 col-lg-1">
-        <input type="text" class="form-control" name="month" placeholder="MM">
+        <input type="text" class="form-control" name="payu_month" placeholder="MM">
     </div>
 
     <div class="col-2 col-sm-2 col-md-2 col-lg-1">
-        <input type="text" class="form-control" name="year" placeholder="YY">
+        <input type="text" class="form-control" name="payu_year" placeholder="YY">
     </div>
 
     <div class="col-2">
-        <select name="network" class="custom-select">
+        <select name="payu_network" class="custom-select">
             <option selected>Select</option>
             <option value="visa">VISA</option>
             <option value="amex">AMEX</option>
@@ -38,10 +38,10 @@
 
 <div class="form-group form-row">
     <div class="col-5">
-        <input type="text" class="form-control" name="name" placeholder="Your Name">
+        <input type="text" class="form-control" name="payu_name" placeholder="Your Name">
     </div>
     <div class="col-5">
-        <input type="email" class="form-control" name="email" placeholder="email@example.com">
+        <input type="email" class="form-control" name="payu_email" placeholder="email@example.com">
     </div>
 </div>
 
